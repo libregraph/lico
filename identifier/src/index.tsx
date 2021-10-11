e0050918a046d7a6e123414ffffe0532536dd194
Simon Eisenmann <simon@longsleep.org>
2021-10-11 16:44:31 +0200

    Update 3rd-party identifier webapp dependencies
    Update includes React 17 and also begins the transition to typescript.

--- identifier/src/index.tsx
+++ identifier/src/index.tsx
@@ -4,12 +4,9 @@ import 'typeface-roboto';
 import './app.css';
 import './fancy-background.css';
 
-import * as kpop from 'kpop/es/version';
-
 import * as version from './version';
 
 console.info(`Kopano Identifier build version: ${version.build}`); // eslint-disable-line no-console
-console.info(`Kopano Kpop build version: ${kpop.build}`); // eslint-disable-line no-console
 
 // NOTE(longsleep): Load async, this enables code splitting via Webpack.
 import(/* webpackChunkName: "identifier-app" */ './app');
