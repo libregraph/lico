libregraph/lico

commit 401fdc1ff0dc25eba052cea29818a729336cdb10

Allow backends to set top level ID token claims
Independ of access token claims, this change makes it possible for
backends to set top level ID token claims. The behavior is added
for the libregraph backend via extension. This change also fixes
the issue that extra claims from the access token were not added
to the ID token.

identifier/backends/libregraph/libregraph.go
@@ -47,6 +47,7 @@ const (
 	OpenTypeExtensionType = "#microsoft.graph.openTypeExtension"
 
 	IdentityClaimsExtensionName    = "libregraph.identityClaims"
+	IDTokenClaimsExtensionName     = "libregraph.idTokenClaims"
 	AccessTokenClaimsExtensionName = "libregraph.accessTokenClaims"
 	RequestedScopesExtensionName   = "libregraph.requestedScopes"
 	SessionExtensionName           = "libregraph.session"
@@ -104,6 +105,7 @@ func decodeLibreGraphUser(r io.Reader) (*libreGraphUser, error) {
 	identityClaims := make(map[string]interface{})
 	identityClaims[konnect.IdentifiedUserIDClaim] = u.ID
 
+	var idTokenClaims map[string]interface{}
 	var accessTokenClaims map[string]interface{}
 	var requestedScopes []string
 
@@ -116,12 +118,21 @@ func decodeLibreGraphUser(r io.Reader) (*libreGraphUser, error) {
 			case IdentityClaimsExtensionName:
 				if v, ok := extension["claims"].(map[string]interface{}); ok {
 					for k, v := range v {
-						if k == "" {
-							// Ignore empty key, its used internally by
-							// AccessTokenClaimsExtensionName.
+						if k == konnect.InternalExtraIDTokenClaimsClaim || k == konnect.InternalExtraAccessTokenClaimsClaim {
+							// Ignore keys which areused internally by IDTokenClaimsExtensionName
+							// and AccessTokenClaimsExtensionName.
 							continue
 						}
 						identityClaims[k] = v
+					}
+				}
+			case IDTokenClaimsExtensionName:
+				if idTokenClaims == nil {
+					idTokenClaims = make(map[string]interface{})
+				}
+				if v, ok := extension["claims"].(map[string]interface{}); ok {
+					for k, v := range v {
+						idTokenClaims[k] = v
 					}
 				}
 			case AccessTokenClaimsExtensionName:
@@ -154,10 +165,16 @@ func decodeLibreGraphUser(r io.Reader) (*libreGraphUser, error) {
 		}
 	}
 
+	if idTokenClaims != nil {
+		// Inject claims as nested identity claim. The key is picket up by the
+		// token signer and used to extend ID token root claims.
+		identityClaims[konnect.InternalExtraIDTokenClaimsClaim] = idTokenClaims
+	}
 	if accessTokenClaims != nil {
-		// Inject root claims as nested identity claims. The empty key is picked
-		// up by the access token signer and used to extend the root claims.
-		identityClaims[""] = accessTokenClaims
+		// Inject claims as nested identity claims. The key is picked up by the
+		// token signer and userinfo handler  to extend ID and access token root
+		// claims based on the request.
+		identityClaims[konnect.InternalExtraAccessTokenClaimsClaim] = accessTokenClaims
 	}
 	if requestedScopes != nil {
 		u.requestedScopes = requestedScopes

claims.go
@@ -44,6 +44,12 @@ const (
 	IdentifiedUserIsGuest      = "gu"
 )
 
+// Internal claim names used for special things.
+const (
+	InternalExtraIDTokenClaimsClaim     = "$lico.id.extra"
+	InternalExtraAccessTokenClaimsClaim = "$lico.at.extra"
+)
+
 // TokenType defines the token type value.
 type TokenTypeValue string
 

oidc/provider/handlers.go
@@ -700,7 +700,7 @@ done:
 	}
 
 	// Inject extra claims.
-	extraClaims := auth.Claims("")[0]
+	extraClaims := auth.Claims(konnect.InternalExtraAccessTokenClaimsClaim)[0]
 	if extraClaims != nil {
 		if extraClaimsMap, ok := extraClaims.(jwt.MapClaims); ok {
 			for claim, value := range extraClaimsMap {

oidc/provider/tokens.go
@@ -72,16 +72,18 @@ func (p *Provider) makeAccessToken(ctx context.Context, audience string, auth id
 	// Support additional custom user specific claims.
 	var finalAccessTokenClaims jwt.Claims = accessTokenClaims
 	if accessTokenClaims.IdentityClaims != nil {
-		// Look for special empty key, if its a map all claims in there are
-		// elevated to top level.
-		extraClaimsMap, _ := accessTokenClaims.IdentityClaims[""].(map[string]interface{})
-		if extraClaimsMap != nil {
-			delete(accessTokenClaims.IdentityClaims, "")
-			accessTokenClaimsMap, err := payload.ToMap(accessTokenClaims)
-			if err != nil {
-				return "", err
-			}
+		accessTokenClaimsMap, err := payload.ToMap(accessTokenClaims)
+		if err != nil {
+			return "", err
+		}
 
+		delete(accessTokenClaimsMap[konnect.IdentityClaim].(map[string]interface{}), konnect.InternalExtraIDTokenClaimsClaim)
+		delete(accessTokenClaimsMap[konnect.IdentityClaim].(map[string]interface{}), konnect.InternalExtraAccessTokenClaimsClaim)
+
+		// Look for special internal key, if its a map all claims in there are
+		// elevated to top level.
+		extraClaimsMap, _ := accessTokenClaims.IdentityClaims[konnect.InternalExtraAccessTokenClaimsClaim].(map[string]interface{})
+		if extraClaimsMap != nil {
 			// Inject extra claims.
 			for claim, value := range extraClaimsMap {
 				switch claim {
@@ -100,9 +102,9 @@ func (p *Provider) makeAccessToken(ctx context.Context, audience string, auth id
 				}
 				accessTokenClaimsMap[claim] = value
 			}
-
-			finalAccessTokenClaims = jwt.MapClaims(accessTokenClaimsMap)
 		}
+
+		finalAccessTokenClaims = jwt.MapClaims(accessTokenClaimsMap)
 	}
 
 	accessToken := jwt.NewWithClaims(sk.SigningMethod, finalAccessTokenClaims)
@@ -133,6 +135,8 @@ func (p *Provider) makeIDToken(ctx context.Context, ar *payload.AuthenticationRe
 		},
 	}
 
+	accessTokenClaims := konnect.AccessTokenClaims{}
+
 	if session != nil {
 		// Include session data in ID token.
 		idTokenClaims.SessionClaims = &konnectoidc.SessionClaims{
@@ -149,6 +153,14 @@ func (p *Provider) makeIDToken(ctx context.Context, ar *payload.AuthenticationRe
 	withAuthTime := ar.MaxAge > 0
 	withIDTokenClaimsRequest := authorizedClaimsRequest != nil && authorizedClaimsRequest.IDToken != nil
 
+	user := auth.User()
+	if user == nil {
+		return "", fmt.Errorf("no user")
+	}
+	if userWithClaims, ok := user.(identity.UserWithClaims); ok {
+		accessTokenClaims.IdentityClaims = userWithClaims.Claims()
+	}
+
 	if withIDTokenClaimsRequest {
 		// Apply additional information from ID token claims request.
 		if _, ok := authorizedClaimsRequest.IDToken.Get(oidc.AuthTimeClaim); !withAuthTime && ok {
@@ -158,15 +170,9 @@ func (p *Provider) makeIDToken(ctx context.Context, ar *payload.AuthenticationRe
 	}
 
 	if !withAccessToken || withIDTokenClaimsRequest {
-		user := auth.User()
-		if user == nil {
-			return "", fmt.Errorf("no user")
-		}
-
 		var userID string
-		if userWithClaims, ok := user.(identity.UserWithClaims); ok {
-			identityClaims := userWithClaims.Claims()
-			if userIDString, ok := identityClaims[konnect.IdentifiedUserIDClaim]; ok {
+		if accessTokenClaims.IdentityClaims != nil {
+			if userIDString, ok := accessTokenClaims.IdentityClaims[konnect.IdentifiedUserIDClaim]; ok {
 				userID = userIDString.(string)
 			}
 		}
@@ -236,31 +242,38 @@ func (p *Provider) makeIDToken(ctx context.Context, ar *payload.AuthenticationRe
 		}
 	}
 
-	// Support extra non-standard claims in ID token.
-	var finalIDTokenClaims jwt.Claims = idTokenClaims
-	if !withAccessToken {
-		// Include requested scope data in ID token when no access token is
-		// generated - additional custom user specific claims.
-		idTokenClaimsMap, err := payload.ToMap(idTokenClaims)
-		if err != nil {
-			return "", err
-		}
+	// To support extra non-standard claims in ID token, convert claim set to
+	// map.
+	idTokenClaimsMap, err := payload.ToMap(idTokenClaims)
+	if err != nil {
+		return "", err
+	}
 
-		// Inject extra claims.
-		extraClaims := auth.Claims("")[0]
-		if extraClaims != nil {
-			if extraClaimsMap, ok := extraClaims.(jwt.MapClaims); ok {
-				for claim, value := range extraClaimsMap {
-					idTokenClaimsMap[claim] = value
-				}
+	if accessTokenClaims.IdentityClaims != nil {
+		// Inject available extra ID token claims.
+		extraClaimsMap, _ := accessTokenClaims.IdentityClaims[konnect.InternalExtraIDTokenClaimsClaim].(map[string]interface{})
+		if extraClaimsMap != nil {
+			for claim, value := range extraClaimsMap {
+				idTokenClaimsMap[claim] = value
 			}
 		}
+	}
 
-		finalIDTokenClaims = jwt.MapClaims(idTokenClaimsMap)
+	if !withAccessToken && accessTokenClaims.IdentityClaims != nil {
+		// Include requested scope data in ID token when no access token is
+		// generated - additional custom user specific claims.
+
+		// Inject extra claims.
+		extraClaimsMap, _ := accessTokenClaims.IdentityClaims[konnect.InternalExtraAccessTokenClaimsClaim].(map[string]interface{})
+		if extraClaimsMap != nil {
+			for claim, value := range extraClaimsMap {
+				idTokenClaimsMap[claim] = value
+			}
+		}
 	}
 
 	// Create signed token.
-	idToken := jwt.NewWithClaims(sk.SigningMethod, finalIDTokenClaims)
+	idToken := jwt.NewWithClaims(sk.SigningMethod, jwt.MapClaims(idTokenClaimsMap))
 	idToken.Header[oidc.JWTHeaderKeyID] = sk.ID
 
 	return idToken.SignedString(sk.PrivateKey)

commit ec8bb68bc3de7a80fc5df936b23a17632d1d2174

Fix access token sid claim when provided via lg backend
It was discovered that when the libregraph backend provides the session
id it was not correctly ending up in the access token sid claim.

This fixes the regression introduced in
401fdc1ff0dc25eba052cea29818a729336cdb10 and makes sid claim usable
again.

identifier/backends/libregraph/libregraph.go
@@ -245,7 +245,7 @@ func (u *libreGraphUser) setRequiredScopes(selectedScope string, scopeMap *order
 }
 
 func (u *libreGraphUser) sessionID() string {
-	if accessTokenClaims, ok := u.identityClaims[""].(map[string]interface{}); ok {
+	if accessTokenClaims, ok := u.identityClaims[konnect.InternalExtraAccessTokenClaimsClaim].(map[string]interface{}); ok {
 		if sessionID, withSessionID := accessTokenClaims[oidc.SessionIDClaim].(string); withSessionID {
 			if sessionID != "" {
 				return sessionID