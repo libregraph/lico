libregraph/lico

commit 95f62c2893447d077dbe5de4d858996d4848d1ce

Add login hint support
Add login hint support as per oidc standard and prepopulate username.

identifier/src/containers/Login/Login.jsx
@@ -81,9 +81,15 @@ function Login(props) {
       }
 
       history.replace(`/chooseaccount${history.location.search}${history.location.hash}`);
-      return;
-    }
-  }, [ /* no dependencies */ ]); // eslint-disable-line react-hooks/exhaustive-deps
+        return;
+      }
+    if (query && query.login_hint) {
+      if (validator.isEmail(query.login_hint) || validator.isEmail(`${query.login_hint}@example.com`)) {
+        // for login hint user name validation
+        dispatch(updateInput("username", query.login_hint));
+      }  
+      }
+    }, [ /* no dependencies */ ]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const handleChange = (name) => (event) => {
     dispatch(updateInput(name, event.target.value));

commit 9a2995b09a85e2d18f002ab925c880af7dbc9e46

Fix missing validator import when login_hint is given
This change fixes a regression from
95f62c2893447d077dbe5de4d858996d4848d1ce which was missing the
corresponding import leading to a javascript error when the login_hint
query parameter is provided by the RP.

identifier/src/containers/Login/Login.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 
 import renderIf from 'render-if';
+import { isEmail } from 'validator';
 
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -83,9 +84,10 @@ function Login(props) {
       history.replace(`/chooseaccount${history.location.search}${history.location.hash}`);
       return;
     }
-    // Login Hint validation is done so as to update user name automatically based on login hint provided.
+
+    // If login_hint is an email, set it into the username field automatically.
     if (query && query.login_hint) {
-      if (validator.isEmail(query.login_hint) || validator.isEmail(`${query.login_hint}@example.com`)) {
+      if (isEmail(query.login_hint) || isEmail(`${query.login_hint}@example.com`)) {
         dispatch(updateInput("username", query.login_hint));
       }  
     }