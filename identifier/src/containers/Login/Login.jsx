libregraph/lico

commit d12fad6bd4ab50bdacfae76e7c6631d845c2fcdc

Add support for visual branding of identifier
This change extends the identfier hello response to include a
branding record which supports custom logo, placeholder and
additional text to show on sign-in pages.

identifier/src/containers/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -12,6 +12,7 @@ import green from '@material-ui/core/colors/green';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
 
 import { updateInput, executeLogonIfFormValid, advanceLogonFlow } from '../../actions/login';
 import { ErrorMessage } from '../../errors';
@@ -36,6 +37,9 @@ const styles = theme => ({
     position: 'relative',
     display: 'inline-block'
   },
+  slideContainer: {
+    overflowX: 'hidden',
+  },
   message: {
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(2)
@@ -44,7 +48,7 @@ const styles = theme => ({
 
 const loginTranslations = defineMessages({
   login_username_placeholder: {
-    id: 'konnect.login.usernameField.label',
+    id: 'konnect.login.usernameField.placeholder',
     defaultMessage: 'Username'
   },
   login_password_placeholder: {
@@ -53,13 +57,23 @@ const loginTranslations = defineMessages({
   },
 });
 
-class Login extends React.PureComponent {
-  state = {};
+function Login(props) {
+  const {
+    hello,
+    query,
+    dispatch,
+    history,
+    loading,
+    errors,
+    classes,
+    username,
+    password,
+    intl,
+  } = props;
 
-  componentDidMount() {
-    const { hello, query, dispatch, history } = this.props;
+  useEffect(() => {
     if (hello && hello.state && history.action !== 'PUSH') {
-      if (!query.prompt || query.prompt.indexOf('select_account') == -1) {
+      if (!query.prompt || query.prompt.indexOf('select_account') === -1) {
         dispatch(advanceLogonFlow(true, history));
         return;
       }
@@ -67,87 +81,82 @@ class Login extends React.PureComponent {
       history.replace(`/chooseaccount${history.location.search}${history.location.hash}`);
       return;
     }
-  }
+  }, [ /* no dependencies */ ]);
 
-  render() {
-    const { loading, errors, classes, username, intl } = this.props;
+  const handleChange = (name) => (event) => {
+    dispatch(updateInput(name, event.target.value));
+  };
 
-    return (
-      <div>
-        <Typography variant="h5" component="h3">
-          <FormattedMessage id="konnect.login.headline" defaultMessage="Sign in"></FormattedMessage>
-        </Typography>
-
-        <form action="" onSubmit={(event) => this.logon(event)}>
-          <div>
-            <TextField
-              placeholder={intl.formatMessage(loginTranslations.login_username_placeholder)}
-              error={!!errors.username}
-              helperText={<ErrorMessage error={errors.username}></ErrorMessage>}
-              fullWidth
-              margin="normal"
-              autoFocus
-              inputProps={{
-                autoCapitalize: 'off',
-                spellCheck: 'false'
-              }}
-              value={username}
-              onChange={this.handleChange('username')}
-              autoComplete="kopano-account username"
-            />
-            <TextField
-              type="password"
-              placeholder={intl.formatMessage(loginTranslations.login_password_placeholder)}
-              error={!!errors.password}
-              helperText={<ErrorMessage error={errors.password}></ErrorMessage>}
-              fullWidth
-              margin="normal"
-              onChange={this.handleChange('password')}
-              autoComplete="kopano-account current-password"
-            />
-            <DialogActions>
-              <div className={classes.wrapper}>
-                <Button
-                  type="submit"
-                  color="primary"
-                  variant="contained"
-                  className={classes.button}
-                  disabled={!!loading}
-                  onClick={(event) => this.logon(event)}
-                >
-                  <FormattedMessage id="konnect.login.nextButton.label" defaultMessage="Next"></FormattedMessage>
-                </Button>
-                {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
-              </div>
-            </DialogActions>
-          </div>
-
-          {renderIf(errors.http)(() => (
-            <Typography variant="subtitle2" color="error" className={classes.message}>
-              <ErrorMessage error={errors.http}></ErrorMessage>
-            </Typography>
-          ))}
-        </form>
-      </div>
-    );
-  }
-
-  handleChange(name) {
-    return event => {
-      this.props.dispatch(updateInput(name, event.target.value));
-    };
-  }
-
-  logon(event) {
+  const handleNextClick = (event) => {
     event.preventDefault();
 
-    const { username, password, dispatch, history } = this.props;
     dispatch(executeLogonIfFormValid(username, password, false)).then((response) => {
       if (response.success) {
         dispatch(advanceLogonFlow(response.success, history));
       }
     });
-  }
+  };
+
+  const usernamePlaceHolder = hello?.details?.branding?.usernameHintText ? hello.details.branding.usernameHintText : intl.formatMessage(loginTranslations.login_username_placeholder);
+
+  return (
+    <DialogContent>
+      <Typography variant="h5" component="h3" gutterBottom>
+        <FormattedMessage id="konnect.login.headline" defaultMessage="Sign in"></FormattedMessage>
+      </Typography>
+
+      <form action="" onSubmit={(event) => this.logon(event)}>
+        <TextField
+          placeholder={usernamePlaceHolder}
+          error={!!errors.username}
+          helperText={<ErrorMessage error={errors.username} values={{what: usernamePlaceHolder.charAt(0).toLowerCase() + usernamePlaceHolder.slice(1)}}></ErrorMessage>}
+          fullWidth
+          margin="dense"
+          autoFocus
+          inputProps={{
+            autoCapitalize: 'off',
+            spellCheck: 'false'
+          }}
+          value={username}
+          onChange={handleChange('username')}
+          autoComplete="kopano-account username"
+        />
+        <TextField
+          type="password"
+          placeholder={intl.formatMessage(loginTranslations.login_password_placeholder)}
+          error={!!errors.password}
+          helperText={<ErrorMessage error={errors.password}></ErrorMessage>}
+          fullWidth
+          margin="dense"
+          onChange={handleChange('password')}
+          autoComplete="kopano-account current-password"
+        />
+        <DialogActions>
+          <div className={classes.wrapper}>
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              className={classes.button}
+              disabled={!!loading}
+              onClick={handleNextClick}
+            >
+              <FormattedMessage id="konnect.login.nextButton.label" defaultMessage="Next"></FormattedMessage>
+            </Button>
+            {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+          </div>
+        </DialogActions>
+
+        {renderIf(errors.http)(() => (
+          <Typography variant="subtitle2" color="error" className={classes.message}>
+            <ErrorMessage error={errors.http}></ErrorMessage>
+          </Typography>
+        ))}
+
+        {hello?.details?.branding?.signinPageText && <Typography variant="body2">{hello.details.branding.signinPageText}</Typography>}
+      </form>
+    </DialogContent>
+  );
 }
 
 Login.propTypes = {

bootstrap/bootstrap.go
@@ -84,6 +84,9 @@ type Config struct {
 	IdentifierClientPath              string
 	IdentifierRegistrationConf        string
 	IdentifierScopesConf              string
+	IdentifierDefaultBannerLogo       string
+	IdentifierDefaultSignInPageText   string
+	IdentifierDefaultUsernameHintText string
 	SigningKid                        string
 	SigningMethod                     string
 	SigningPrivateKeyFiles            []string
@@ -112,12 +115,16 @@ type bootstrap struct {
 
 	tlsClientConfig *tls.Config
 
-	issuerIdentifierURI        *url.URL
-	identifierClientDisabled   bool
-	identifierClientPath       string
-	identifierRegistrationConf string
-	identifierAuthoritiesConf  string
-	identifierScopesConf       string
+	issuerIdentifierURI *url.URL
+
+	identifierClientDisabled          bool
+	identifierClientPath              string
+	identifierRegistrationConf        string
+	identifierAuthoritiesConf         string
+	identifierScopesConf              string
+	identifierDefaultBannerLogo       []byte
+	IdentifierDefaultSignInPageText   *string
+	IdentifierDefaultUsernameHintText *string
 
 	encryptionSecret []byte
 	signingMethod    jwt.SigningMethod
@@ -300,6 +307,21 @@ func (bs *bootstrap) initialize(cfg *Config) error {
 		}
 	}
 
+	if cfg.IdentifierDefaultBannerLogo != "" {
+		// Load from file.
+		b, errRead := ioutil.ReadFile(cfg.IdentifierDefaultBannerLogo)
+		if errRead != nil {
+			return fmt.Errorf("identifier-default-banner-logo failed to open: %w", errRead)
+		}
+		bs.identifierDefaultBannerLogo = b
+	}
+	if cfg.IdentifierDefaultSignInPageText != "" {
+		bs.IdentifierDefaultSignInPageText = &cfg.IdentifierDefaultSignInPageText
+	}
+	if cfg.IdentifierDefaultUsernameHintText != "" {
+		bs.IdentifierDefaultUsernameHintText = &cfg.IdentifierDefaultUsernameHintText
+	}
+
 	bs.signingKeyID = cfg.SigningKid
 	bs.signers = make(map[string]crypto.Signer)
 	bs.validators = make(map[string]crypto.PublicKey)
@@ -457,13 +479,13 @@ func (bs *bootstrap) setupIdentity(ctx context.Context, cfg *Config) (identity.M
 		identityManager, err = newCookieIdentityManager(bs, cfg)
 
 	case identityManagerNameKC:
-		identityManager, err = newKCIdentityManager(bs)
+		identityManager, err = newKCIdentityManager(bs, cfg)
 
 	case identityManagerNameLDAP:
-		identityManager, err = newLDAPIdentityManager(bs)
+		identityManager, err = newLDAPIdentityManager(bs, cfg)
 
 	case identityManagerNameDummy:
-		identityManager, err = newDummyIdentityManager(bs)
+		identityManager, err = newDummyIdentityManager(bs, cfg)
 
 	default:
 		err = fmt.Errorf("unknown identity manager %v", identityManagerName)

bootstrap/dummy.go
@@ -22,7 +22,7 @@ import (
 	identityManagers "github.com/libregraph/lico/identity/managers"
 )
 
-func newDummyIdentityManager(bs *bootstrap) (identity.Manager, error) {
+func newDummyIdentityManager(bs *bootstrap, cfg *Config) (identity.Manager, error) {
 	logger := bs.cfg.Logger
 
 	identityManagerConfig := &identity.Config{

bootstrap/kc.go
@@ -34,7 +34,7 @@ import (
 	"github.com/libregraph/lico/version"
 )
 
-func newKCIdentityManager(bs *bootstrap) (identity.Manager, error) {
+func newKCIdentityManager(bs *bootstrap, cfg *Config) (identity.Manager, error) {
 	logger := bs.cfg.Logger
 
 	if bs.authorizationEndpointURI.String() != "" {
@@ -137,6 +137,10 @@ func newKCIdentityManager(bs *bootstrap) (identity.Manager, error) {
 
 		AuthorizationEndpointURI: fullAuthorizationEndpointURL,
 		SignedOutEndpointURI:     fullSignedOutEndpointURL,
+
+		DefaultBannerLogo:       bs.identifierDefaultBannerLogo,
+		DefaultSignInPageText:   bs.IdentifierDefaultSignInPageText,
+		DefaultUsernameHintText: bs.IdentifierDefaultUsernameHintText,
 
 		Backend: identifierBackend,
 	})

bootstrap/ldap.go
@@ -29,7 +29,7 @@ import (
 	identityManagers "github.com/libregraph/lico/identity/managers"
 )
 
-func newLDAPIdentityManager(bs *bootstrap) (identity.Manager, error) {
+func newLDAPIdentityManager(bs *bootstrap, cfg *Config) (identity.Manager, error) {
 	logger := bs.cfg.Logger
 
 	if bs.authorizationEndpointURI.String() != "" {
@@ -102,6 +102,10 @@ func newLDAPIdentityManager(bs *bootstrap) (identity.Manager, error) {
 
 		AuthorizationEndpointURI: fullAuthorizationEndpointURL,
 		SignedOutEndpointURI:     fullSignedOutEndpointURL,
+
+		DefaultBannerLogo:       bs.identifierDefaultBannerLogo,
+		DefaultSignInPageText:   bs.IdentifierDefaultSignInPageText,
+		DefaultUsernameHintText: bs.IdentifierDefaultUsernameHintText,
 
 		Backend: identifierBackend,
 	})

cmd/licod/serve.go
@@ -88,6 +88,9 @@ func commandServe() *cobra.Command {
 	serveCmd.Flags().StringVar(&cfg.IdentifierClientPath, "identifier-client-path", envOrDefault("LICOD_IDENTIFIER_CLIENT_PATH", defaultIdentifierClientPath), fmt.Sprintf("Path to the identifier web client base folder (default \"%s\")", defaultIdentifierClientPath))
 	serveCmd.Flags().StringVar(&cfg.IdentifierRegistrationConf, "identifier-registration-conf", "", "Path to a identifier-registration.yaml configuration file")
 	serveCmd.Flags().StringVar(&cfg.IdentifierScopesConf, "identifier-scopes-conf", "", "Path to a scopes.yaml configuration file")
+	serveCmd.Flags().StringVar(&cfg.IdentifierDefaultBannerLogo, "identifier-default-banner-logo", "", "Path to a default banner logo that appears on sign-in page.")
+	serveCmd.Flags().StringVar(&cfg.IdentifierDefaultSignInPageText, "identifier-default-sign-in-page-text", "", "Default text that appears at the bottom of the sign-in box.")
+	serveCmd.Flags().StringVar(&cfg.IdentifierDefaultUsernameHintText, "identifier-default-username-hint-text", "", "Default string that shows as the hint in the username textbox on the sign-in screen.")
 	serveCmd.Flags().BoolVar(&cfg.Insecure, "insecure", false, "Disable TLS certificate and hostname validation")
 	serveCmd.Flags().StringArrayVar(&cfg.TrustedProxy, "trusted-proxy", nil, "Trusted proxy IP or IP network (can be used multiple times)")
 	serveCmd.Flags().StringArrayVar(&cfg.AllowScope, "allow-scope", nil, "Allow OAuth 2 scope (can be used multiple times, if not set default scopes are allowed)")

go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/crewjam/saml v0.4.5
 	github.com/deckarep/golang-set v1.7.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gabriel-vasile/mimetype v1.4.0
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-ldap/ldap/v3 v3.4.1
 	github.com/google/go-querystring v1.1.0

go.sum
@@ -99,6 +99,8 @@ github.com/eternnoir/gncp v0.0.0-20170707042257-c70df2d0cd68/go.mod h1:8FuQ7lU9Z
 github.com/fatih/color v1.7.0/go.mod h1:Zm6kSWBoL9eyXnKyktHP6abPY2pDugNf5KwzbycvMj4=
 github.com/fsnotify/fsnotify v1.4.7/go.mod h1:jwhsz4b93w/PPRr/qN1Yymfu8t87LnFCMoQvtojpjFo=
 github.com/fsnotify/fsnotify v1.4.9/go.mod h1:znqG4EE+3YCdAaPaxE2ZRY/06pZUdp0tY4IgpuI1SZQ=
+github.com/gabriel-vasile/mimetype v1.4.0 h1:Cn9dkdYsMIu56tGho+fqzh7XmvY2YyGU0FnbhiOsEro=
+github.com/gabriel-vasile/mimetype v1.4.0/go.mod h1:fA8fi6KUiG7MgQQ+mEWotXoEOvmxRtOJlERCzSmRvr8=
 github.com/ghodss/yaml v1.0.0 h1:wQHKEahhL6wmXdzwWG11gIVCkOv05bNOh+Rxn0yngAk=
 github.com/ghodss/yaml v1.0.0/go.mod h1:4dBDuWmgqj2HViK6kFavaiC9ZROes6MMH2rRYeMEF04=
 github.com/gin-contrib/sse v0.1.0/go.mod h1:RHrZQHXnP2xjPF+u1gW/2HnVO7nvIa9PG3Gm+fLHvGI=
@@ -473,6 +475,7 @@ golang.org/x/net v0.0.0-20210119194325-5f4716e94777/go.mod h1:m0MpNAwzfU5UDzcl9v
 golang.org/x/net v0.0.0-20210226172049-e18ecbb05110/go.mod h1:m0MpNAwzfU5UDzcl9v0D8zg8gWTRqZa9RBIspLL5mdg=
 golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4/go.mod h1:RBQZq4jEuRlivfhVLdyRGr576XBO4/greRjx4P4O3yc=
 golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4/go.mod h1:p54w0d4576C0XHj96bSt6lcn1PtDYWL6XObtHCRCNQM=
+golang.org/x/net v0.0.0-20210505024714-0287a6fb4125/go.mod h1:9nx3DQGgdP8bBQD5qxJ1jj9UTztislL4KSBs9R2vV5Y=
 golang.org/x/net v0.0.0-20211008194852-3b03d305991f h1:1scJEYZBaF48BaG6tYbtxmLcXqwYGSfGcMoStTqkkIw=
 golang.org/x/net v0.0.0-20211008194852-3b03d305991f/go.mod h1:9nx3DQGgdP8bBQD5qxJ1jj9UTztislL4KSBs9R2vV5Y=
 golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be/go.mod h1:N/0e6XlmueqKjAGxoOufVs8QHGRruUQn6yWY3a++T0U=

commit 647eaffe91b5c917b2c2a04b159eb70ef2f38bb5

Fix form submission handler regression introduced in class-to-functional conversion
This fixes a regression introduced in commit d12fad6bd4ab50bdacfae76e7c6631d845c2fcdc
'Add support for visual branding of identifier' where the Login component was
converted from class-based to functional component.

During the conversion:
- The 'logon' method was correctly renamed to 'handleNextClick'
- The button's onClick was correctly updated to use 'handleNextClick'
- However, the form's onSubmit was left with the old 'this.logon(event)' reference

This resulted in a broken form submission handler that referenced a non-existent
method. The application continued to work because Enter key presses trigger the
submit button's onClick handler due to standard HTML form behavior, masking
the regression.

Changes:
- Replace broken 'onSubmit={(event) => this.logon(event)}' with 'onSubmit={handleNextClick}'
- Ensures consistent behavior between form submission and button click
- Maintains existing preventDefault behavior and proper form handling

Regression introduced: d12fad6bd4ab50bdacfae76e7c6631d845c2fcdc (Oct 12, 2021)
Component: identifier/src/containers/Login/Login.jsx:137

identifier/src/containers/Login/Login.jsx
@@ -134,7 +134,7 @@ function Login(props) {
         {t("konnect.login.headline", "Sign in")}
       </Typography>
 
-      <form action="" onSubmit={(event) => this.logon(event)}>
+      <form action="" onSubmit={handleNextClick}>
         <TextField
           label={usernamePlaceHolder}
           error={!!errors.username}